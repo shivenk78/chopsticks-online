39852e92ca49764004482c61289f5d7e53efbe2d
Abhishek Yadav <abhishekyad1098@gmail.com>
2018-09-09 01:35:53 -0400

    All game variables, objects set up ready to use.

--- logic.js
+++ logic.js
@@ -27,39 +27,6 @@ class Player{
   }
 }
 
-function turn(user, comp){
-  if(gameTurn == GameTurn.USER){
-    var move = prompt("L/R/S?");
-    var side = prompt("What side do you want to use?R/L");
-    if(move==='L'){
-      if(side === 'R'){ user.rightHand.hit(comp.leftHand)}
-      if(side === 'L'){ user.leftHand.hit(comp.leftHand)}
-    }else if(move === 'R'){
-      if(side === 'R'){ user.rightHand.hit(comp.rightHand)}
-      if(side === 'L'){ user.leftHand.hit(comp.rightHand)}
-    }else{
-
-    }
-    gameTurn = GameTurn.COMP;
-    clearInterval(refreshIntervalId);
-  }else{
-    var move = prompt("L/R/S?");
-    var side = prompt("What side do you want to use?R/L");
-    if(move==='L'){
-      if(side === 'R'){ comp.rightHand.hit(user.leftHand)}
-      if(side === 'L'){ comp.leftHand.hit(user.leftHand)}
-    }else if(move === 'R'){
-      if(side === 'R'){ comp.rightHand.hit(user.rightHand)}
-      if(side === 'L'){ comp.leftHand.hit(user.rightHand)}
-    }else{
-
-    }
-    gameTurn = GameTurn.USER;
-    clearInterval(refreshIntervalId);
-  }
-}
-
-
 var GameStatus = Object.freeze({"RUNNING":1, "OVER":2, "PAUSED":3});
 var GameTurn = Object.freeze({"USER":1, "COMP":2});
 var turnCounter = 0;
@@ -70,26 +37,14 @@ comp = new Player("COMP");
 ((Math.floor(Math.random()*2)+1) == 1) ? gameTurn = GameTurn.USER : gameTurn = GameTurn.COMP;
 var status = GameStatus.RUNNING;
 
-if(status == GameStatus.RUNNING){
-  var refreshIntervalId = setInterval(turn, 10000);
+//THIS SECTION IS FOR CHECKING IF USER IS CHEATING/PLAYING
+var leapRightFingerCount; //RECIEVE INFO FROM LEAP
+//console.log(user.rightHand);
+if(user.rightHand.fingerCount != leapRightFingerCount){
+  status = GameStatus.PAUSED;
 }
 
-  //THIS SECTION IS FOR CHECKING IF USER IS CHEATING/PLAYING
-  var leapRightFingerCount; //RECIEVE INFO FROM LEAP
-  //console.log(user.rightHand);
-  if(user.rightHand.fingerCount != leapRightFingerCount){
-    status = GameStatus.PAUSED;
-  }
-
-  var leapLeftFingerCount;//RECIEVE INFO FROM LEAP
-  if(user.leftHand.fingerCount != leapLeftFingerCount){
-    status = GameStatus.PAUSED;
-  }
-
-  turnCounter++;
-  if(turnCounter == 10){
-    status = GameStatus.PAUSED;
-  }
-
-  if(status = GameStatus.PAUSED){
-  }
+var leapLeftFingerCount;//RECIEVE INFO FROM LEAP
+if(user.leftHand.fingerCount != leapLeftFingerCount){
+  status = GameStatus.PAUSED;
+}
