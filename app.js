3b4a1b73b2ea0b8b69d8cdb70fb62113041e0a73
Shiven Kumar <kumar.shiven15@gmail.com>
2018-09-09 01:13:22 -0400

    Leap Interface essentially complete

--- app.js
+++ app.js
@@ -14,6 +14,34 @@ var rightFingers = 0;
 var leftFingerNums = [];
 var rightFingerNums = [];
 
+function ChopstickHand(user, side, fingerCount, color){
+    this.user = user;
+    this.side = side;
+    this.fingerCount = fingerCount;
+    this.color = color;
+} 
+function updateHand(hand, newFingerCount){
+    hand.fingerCount = newFingerCount;
+}
+function addToHand(hand, fingerToAdd){
+    hand.fingerCount+=fingerToAdd;
+}
+function changeHandColor(hand, newColor){
+    hand.color = newColor;
+}
+function activeHand(){
+    if(playerLeftHand.color=="blue")
+        return "left";
+    if(playerRightHand.color=="blue")
+        return "right";
+    return null;
+}
+
+var enemyLeftHand = new ChopstickHand("enemy", "left", 1, "red");
+var enemyRightHand = new ChopstickHand("enemy", "right", 1, "red");
+var playerLeftHand = new ChopstickHand("player", "left", leftFingers, "green");
+var playerRightHand = new ChopstickHand("player", "right", rightFingers, "green");
+
 Leap.loop(controllerOptions, function(frame) {
   if (paused) {
     return; // Skip this update
@@ -71,38 +99,62 @@ Leap.loop(controllerOptions, function(frame) {
     }
     for (var i = 0; i < frame.gestures.length; i++) {
       var gesture = frame.gestures[i];
-      gestureString += "Gesture ID: " + gesture.id + ", "
-                    + "type: " + gesture.type + ", "
-                    + "state: " + gesture.state + ", "
-                    + "hand IDs: " + gesture.handIds.join(", ") + ", "
-                    + "pointable IDs: " + gesture.pointableIds.join(", ") + ", "
-                    + "duration: " + gesture.duration + " &micro;s, ";
+    //   gestureString += "Gesture ID: " + gesture.id + ", "
+    //                 + "type: " + gesture.type + ", "
+    //                 + "state: " + gesture.state + ", "
+    //                 + "hand IDs: " + gesture.handIds.join(", ") + ", "
+    //                 + "pointable IDs: " + gesture.pointableIds.join(", ") + ", "
+    //                 + "duration: " + gesture.duration + " &micro;s, ";
 
       switch (gesture.type) {
         case "circle":
-          gestureString += "center: " + vectorToString(gesture.center) + " mm, "
-                        + "normal: " + vectorToString(gesture.normal, 2) + ", "
-                        + "radius: " + gesture.radius.toFixed(1) + " mm, "
-                        + "progress: " + gesture.progress.toFixed(2) + " rotations";
+            gestureString +="CIRCLE";
+                        // += "center: " + vectorToString(gesture.center) + " mm, "
+                        // + "normal: " + vectorToString(gesture.normal, 2) + ", "
+                        // + "radius: " + gesture.radius.toFixed(1) + " mm, "
+                        // + "progress: " + gesture.progress.toFixed(2) + " rotations";
+            var circleHand = frame.hand(gesture.handIds[0]);
+            var circleHandType = circleHand.type;
+            if(circleHandType=="left"){
+                changeHandColor(playerLeftHand, "blue");
+                changeHandColor(playerRightHand, "green");
+            }else{
+                changeHandColor(playerRightHand, "blue");
+                changeHandColor(playerLeftHand, "green");
+            }
           break;
         case "swipe":
-          gestureString += "start position: " + vectorToString(gesture.startPosition) + " mm, "
-                        + "current position: " + vectorToString(gesture.position) + " mm, "
-                        + "direction: " + vectorToString(gesture.direction, 1) + ", "
-                        + "speed: " + gesture.speed.toFixed(1) + " mm/s";
+        //   gestureString += "start position: " + vectorToString(gesture.startPosition) + " mm, "
+        //                 + "current position: " + vectorToString(gesture.position) + " mm, "
+        //                 + "direction: " + vectorToString(gesture.direction, 1) + ", "
+        //                 + "speed: " + gesture.speed.toFixed(1) + " mm/s";
           break;
         case "screenTap":
+            gestureString+="Direction: "+vectorToString(gesture.direction,3)
+                            +" Position: "+vectorToString(gesture.position,3)
+                            +" Duration: "+gesture.duration;
+            var currentHandType = activeHand();
+            console.log(currentHandType);
+            var targetDir = (gesture.direction[0]>0) ? "left" : "right";
+            if( currentHandType=="right" || currentHandType=="left" ){
+                if(targetDir=="left"){
+                    console.log("TARGET LEFT");
+                    (currentHandType=="left") ? addToHand(enemyLeftHand, playerLeftHand.fingerCount) : addToHand(enemyLeftHand, playerRightHand.fingerCount);
+                    (currentHandType=="left") ? changeHandColor(playerLeftHand, "green") : changeHandColor(playerRightHand, "green");
+                }else{
+                    console.log("TARGET RIGHT");
+                    (currentHandType=="left") ? addToHand(enemyRightHand, playerLeftHand.fingerCount) : addToHand(enemyRightHand, playerRightHand.fingerCount);
+                    (currentHandType=="left") ? changeHandColor(playerLeftHand, "green") : changeHandColor(playerRightHand, "green");
+                }
+            }
         case "keyTap":
-          gestureString += "position: " + vectorToString(gesture.position) + " mm";
+          //gestureString += "position: " + vectorToString(gesture.position) + " mm";
           break;
         default:
           gestureString += "unkown gesture type";
       }
-      gestureString += "<br />";
+      //gestureString += "<br />";
     }
-  }
-  else {
-    gestureString += "No gestures";
   }
   console.log(gestureString);
   // Store frame for motion functions
@@ -118,23 +170,9 @@ function vectorToString(vector, digits) {
              + vector[2].toFixed(digits) + ")";
 }
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////DRAWING SECTION//////////////////////////////////////////////////////////////
     var canvas = document.getElementById("myCanvas");
-    var ctx = canvas.getContext("2d");
-
-    function ChopstickHand(user, side, fingerCount){
-        this.user = user;
-        this.side = side;
-        this.fingerCount = fingerCount;
-    } 
-    function updateHand(hand, newFingerCount){
-        hand.fingerCount = newFingerCount;
-    }
-
-    var enemyLeftHand = new ChopstickHand("enemy", "left", 1);
-    var enemyRightHand = new ChopstickHand("enemy", "right", 1);
-    var playerLeftHand = new ChopstickHand("player", "left", leftFingers);
-    var playerRightHand = new ChopstickHand("player", "right", rightFingers);
+    var ctx = canvas.getContext("2d")
 
     function drawEnemyHands(){
         //left palm
@@ -184,14 +222,14 @@ function vectorToString(vector, digits) {
         //left palm
         ctx.beginPath();
         ctx.rect(120,360,80,80);
-        ctx.fillStyle = "green";
+        ctx.fillStyle = playerLeftHand.color;
         ctx.fill();
         ctx.closePath();
 
         //right palm
         ctx.beginPath();
         ctx.rect(280,360,80,80);
-        ctx.fillStyle = "green";
+        ctx.fillStyle = playerRightHand.color;
         ctx.fill();
         ctx.closePath();
 
@@ -203,7 +241,7 @@ function vectorToString(vector, digits) {
             initX = (fing>0) ? 215-(20*fing) : 215;
             ctx.beginPath();
             ctx.rect(initX,initY,-15,-50);
-            ctx.fillStyle = "green";
+            ctx.fillStyle = playerLeftHand.color;
             ctx.fill();
             ctx.closePath();
         }
@@ -216,7 +254,7 @@ function vectorToString(vector, digits) {
             initX = (fing>0) ? 265+(20*fing) : 265;
             ctx.beginPath();
             ctx.rect(initX,initY,15,-50);
-            ctx.fillStyle = "green";
+            ctx.fillStyle = playerRightHand.color;
             ctx.fill();
             ctx.closePath();
         }
